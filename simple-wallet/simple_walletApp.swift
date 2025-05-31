27f126044e507e83f595b158130927ddc4c7d782
kehinde <bankolek1@gmail.com>
2025-05-31 20:54:50 +0100

    Initial Commit

---
+++ simple-wallet/simple_walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_walletApp.swift
+//  simple-wallet
+//
+//  Created by Kehinde Bankole on 31/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct simple_walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ simple-walletTests/simple_walletTests.swift
@@ -0,0 +1,17 @@
+//
+//  simple_walletTests.swift
+//  simple-walletTests
+//
+//  Created by Kehinde Bankole on 31/05/2025.
+//
+
+import Testing
+@testable import simple_wallet
+
+struct simple_walletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
